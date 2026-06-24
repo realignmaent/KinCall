47cc2ee705f4f0b8bc809e8a137e2cbcc908d9f3
realignmaent <yingjieji09@gmail.com>
2026-06-24 22:04:22 +0800

    fix: 修复 settings.gradle.kts dependencyResolution 语法错误

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,17 +1,13 @@
 pluginManagement {
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
+        google()
         mavenCentral()
         gradlePluginPortal()
     }
 }
-dependencyResolution {
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
